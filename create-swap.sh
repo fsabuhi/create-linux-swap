9f46dfcac34a49932db63f8a1fc0c15de9c7d12e
Sabuhi Fatalizada <sabuhif@icloud.com>
2023-09-07 12:49:16 +0000

    Added comments

--- create-swap.sh
+++ create-swap.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
+
+# Prompt the user for the amount of memory to allocate for the swap file
 read -p 'How much MB to allocate?: ' size
-$size = $size / 1024
+
+# Convert the size from MB to GB
+size=$(($size / 1024))
+
+# Allocate the swap file with the specified size
 sudo fallocate -l $size G /swapfile
+
+# Set the correct permissions for the swap file
 sudo chmod 600 /swapfile
+
+# Create a swap area on the file
 sudo mkswap /swapfile
+
+# Enable the swap file
 sudo swapon /swapfile
+
+# Show the current swap usage
 sudo swapon --show
